6e91468d33cc0687a7373ccc07aac10deb117008
0918nobita <nobita.0918@gmail.com>
2019-11-13 08:55:43 +0900

    :wrench: ESLint の設定ファイルを追加

---
+++ punk-webpack-loader/.eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+    'env': {
+        'commonjs': true,
+        'es6': true,
+        'node': true
+    },
+    'extends': 'eslint:recommended',
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
+    },
+    'parserOptions': {
+        'ecmaVersion': 2018,
+        'sourceType': 'module'
+    },
+    'rules': {
+        'indent': [
+            'error',
+            2
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        'no-unused-vars': [
+            2,
+            { argsIgnorePattern: '^_' }
+        ]
+    }
+};
