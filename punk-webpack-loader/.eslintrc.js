bf9ca87c1f109461ef8a3b27a45576f37841fe1c
0918nobita <nobita.0918@gmail.com>
2019-11-13 09:17:50 +0900

    :wrench: Prettier と ESLint の連携設定を追加

--- punk-webpack-loader/.eslintrc.js
+++ punk-webpack-loader/.eslintrc.js
@@ -4,11 +4,15 @@ module.exports = {
         'es6': true,
         'node': true
     },
-    'extends': 'eslint:recommended',
+    'extends': [
+        'eslint:recommended',
+        'plugin:prettier/recommended'
+    ],
     'globals': {
         'Atomics': 'readonly',
         'SharedArrayBuffer': 'readonly'
     },
+    'plugins': ['prettier'],
     'parserOptions': {
         'ecmaVersion': 2018,
         'sourceType': 'module'
