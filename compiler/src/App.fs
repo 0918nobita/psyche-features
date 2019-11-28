5aca8780d74976e80f68befb33b66cf8db81306c
0918nobita <nobita.0918@gmail.com>
2019-11-28 19:06:46 +0900

    :sparkles: ESModule を F# プログラムでインポートするサンプルを追加
    path モジュールの basename 関数を呼び出す

--- compiler/src/App.fs
+++ compiler/src/App.fs
@@ -1,3 +1,13 @@
 module Punk
 
+open Fable.Core
+
+type IPathModule =
+    abstract basename: string -> string
+
+[<ImportDefault("path")>]
+let pathModule: IPathModule = jsNative
+
+printfn "%s" (pathModule.basename "/foo/bar/baz/myfile.html")
+
 printfn "%i" (3 + 4)
