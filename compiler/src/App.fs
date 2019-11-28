21d57eb976066f096a803a54eabde8d604b3b784
0918nobita <nobita.0918@gmail.com>
2019-11-29 01:53:48 +0900

    :sparkles: JS の AST を F# 側で組み立てるサンプルを追加

--- compiler/src/App.fs
+++ compiler/src/App.fs
@@ -12,11 +12,54 @@ let parse: string -> obj = jsNative
 [<ImportDefault("@babel/generator")>]
 let generate (ast: obj, options: obj, code: string): obj = jsNative
 
-let code = "console.log(6 * 7);"
-
-let ast = parse code
-printfn "%s" (JSON.stringify ast)
-
 open Fable.Core.JsInterop
 
-console.log (generate (ast, createObj [], code))
+let file (program : obj) = createObj [
+    "type" ==> "File"
+    "program" ==> program
+]
+
+let program (stmts : obj []) = createObj [
+    "type" ==> "Program"
+    "body" ==> stmts
+]
+
+let exprStmt (expr : obj) = createObj [
+    "type" ==> "ExpressionStatement"
+    "expression" ==> expr
+]
+
+let callExpr (callee : obj) (args : obj []) = createObj [
+    "type" ==> "CallExpression"
+    "callee" ==> callee
+    "arguments" ==> args
+]
+
+let memExpr (object : obj) (property : obj) = createObj [
+    "type" ==> "MemberExpression"
+    "object" ==> object
+    "property" ==> property
+]
+
+let ident (name : string) = createObj [
+    "type" ==> "Identifier"
+    "name" ==> name
+]
+
+let numLiteral (n : int) = createObj [
+    "type" ==> "NumericLiteral"
+    "value" ==> n
+]
+
+let ast =
+    [| exprStmt
+        (callExpr
+            (memExpr
+                (ident "console")
+                (ident "log"))
+            [| numLiteral 42 |])
+    |]
+    |> program
+    |> file
+
+console.log (generate (ast, createObj [], ""))
