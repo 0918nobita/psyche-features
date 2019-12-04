e1f9f64d9e6891c044a60f8f90dfc34fe7de7a31
0918nobita <nobita.0918@gmail.com>
2019-12-05 01:05:04 +0900

    :building_construction: AST の定義を Ast モジュールに分離

--- compiler/src/App.fs
+++ compiler/src/App.fs
@@ -11,56 +11,7 @@ open NodeJS
 
 console.dir _process.argv
 
-open Fable.Core.JsInterop
-
-type Ast =
-    | File of program : Ast
-    | Program of body : Ast []
-    | ExprStmt of expr : Ast
-    | CallExpr of callee : Ast * args : Ast []
-    | MemberExpr of object : Ast * property : Ast
-    | Ident of name : string
-    | NumLiteral of value : int
-
-    member this.JsObject =
-        match this with
-        | File program ->
-            createObj [
-                "type" ==> "File"
-                "program" ==> program.JsObject
-            ]
-        | Program body ->
-            createObj [
-                "type" ==> "Program"
-                "body" ==> Array.map<Ast, obj> (fun x -> x.JsObject) body
-            ]
-        | ExprStmt expr ->
-            createObj [
-                "type" ==> "ExpressionStatement"
-                "expression" ==> expr.JsObject
-            ]
-        | CallExpr (callee, args) ->
-            createObj [
-                "type" ==> "CallExpression"
-                "callee" ==> callee.JsObject
-                "arguments" ==> Array.map<Ast, obj> (fun x -> x.JsObject) args
-            ]
-        | MemberExpr (object, property) ->
-            createObj [
-                "type" ==> "MemberExpression"
-                "object" ==> object.JsObject
-                "property" ==> property.JsObject
-            ]
-        | Ident name ->
-            createObj [
-                "type" ==> "Identifier"
-                "name" ==> name
-            ]
-        | NumLiteral value ->
-            createObj [
-                "type" ==> "NumericLiteral"
-                "value" ==> value
-            ]
+open Ast
 
 let ast =
     File (Program
@@ -70,5 +21,7 @@ let ast =
                 , [| NumLiteral 42 |]
                 ))
         |])
+
+open Fable.Core.JsInterop
 
 let source = generate (ast.JsObject, createObj [], "")
---
+++ compiler/src/Ast.fs
@@ -0,0 +1,52 @@
+module Ast
+
+open Fable.Core.JsInterop
+
+type Ast =
+    | File of program : Ast
+    | Program of body : Ast []
+    | ExprStmt of expr : Ast
+    | CallExpr of callee : Ast * args : Ast []
+    | MemberExpr of object : Ast * property : Ast
+    | Ident of name : string
+    | NumLiteral of value : int
+
+    member this.JsObject =
+        match this with
+        | File program ->
+            createObj [
+                "type" ==> "File"
+                "program" ==> program.JsObject
+            ]
+        | Program body ->
+            createObj [
+                "type" ==> "Program"
+                "body" ==> Array.map<Ast, obj> (fun x -> x.JsObject) body
+            ]
+        | ExprStmt expr ->
+            createObj [
+                "type" ==> "ExpressionStatement"
+                "expression" ==> expr.JsObject
+            ]
+        | CallExpr (callee, args) ->
+            createObj [
+                "type" ==> "CallExpression"
+                "callee" ==> callee.JsObject
+                "arguments" ==> Array.map<Ast, obj> (fun x -> x.JsObject) args
+            ]
+        | MemberExpr (object, property) ->
+            createObj [
+                "type" ==> "MemberExpression"
+                "object" ==> object.JsObject
+                "property" ==> property.JsObject
+            ]
+        | Ident name ->
+            createObj [
+                "type" ==> "Identifier"
+                "name" ==> name
+            ]
+        | NumLiteral value ->
+            createObj [
+                "type" ==> "NumericLiteral"
+                "value" ==> value
+            ]
