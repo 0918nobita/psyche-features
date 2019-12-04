8e3f39885a234a6ac63bc5c443a4de1838cafd05
0918nobita <nobita.0918@gmail.com>
2019-12-05 00:45:06 +0900

    :wrench: nodejs 向けにバンドルされるように webpack の設定を修正

--- compiler/webpack.config.js
+++ compiler/webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: './src/index.ts',
+  target: 'node',
   output: {
     path: path.join(__dirname, './dist'),
     filename: 'bundle.js',
