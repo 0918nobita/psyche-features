83a616025a3a63b13058eedb26358bc43b7fbb58
0918nobita <nobita.0918@gmail.com>
2019-12-17 22:07:52 +0900

    :sparkles: Rx.from に関するサンプルを追加

--- compiler/stream.ts
+++ compiler/stream.ts
@@ -12,5 +12,19 @@ streamA$.subscribe(
 // 配列を渡した場合、先頭から順に要素を流す Cold Observable を作る
 const streamB$ = Rx.from([10, 20, 30]);
 
+// Promise を渡した場合、それを解決して得られた値を流す Cold Observable を作る
+// Promise が解決された後に subscribe しても値は流れる
+const timer = Rx.from(new Promise<string>(resolve => {
+  setTimeout(() => resolve("2秒経過"), 2000);
+}));
+
+timer.subscribe(v => console.log('Log(timer):', v));
+
+/*
+setTimeout(() => {
+  timer.subscribe(v => console.log('Log(timer2):', v));
+}, 4000);
+*/
+
 Rx.combineLatest(streamA$, streamB$)
   .subscribe((v: [number, number]) => console.log('next', v));
