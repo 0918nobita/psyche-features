72079f397e578fa62ea67839a168eebb51a1f35e
0918nobita <nobita.0918@gmail.com>
2019-12-17 19:31:13 +0900

    :sparkles: .punk-cache ディレクトリが存在しなければ生成するようにした

--- compiler/src/index.ts
+++ compiler/src/index.ts
@@ -1,3 +1,10 @@
+import fs from 'fs';
+
 import { source } from './App.fsproj';
 
 console.log(source.code);
+
+// キャッシュの書き込み先ディレクトリが存在しなければ生成する
+if (fs.existsSync('.punk-cache') === false) {
+  fs.mkdirSync('.punk-cache');
+}
