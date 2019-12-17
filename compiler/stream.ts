0269b00926454d2826166c1e1ced7f15709f4542
0918nobita <nobita.0918@gmail.com>
2019-12-17 22:07:29 +0900

    :sparkles: subscribe に関するサンプルを追加

--- compiler/stream.ts
+++ compiler/stream.ts
@@ -3,6 +3,13 @@ import Rx = require('rxjs');
 // ひとつの値が流れるだけの Cold Observable を作る
 const streamA$ = Rx.of(42);
 
+streamA$.subscribe(
+  x => console.log('next:', x),   // onNext: 値が流れてくる
+  err => console.error(err),      // onError: ストリームでのエラー
+  () => console.log('completed')  // onComplete: ストリームの「終了」
+);
+
+// 配列を渡した場合、先頭から順に要素を流す Cold Observable を作る
 const streamB$ = Rx.from([10, 20, 30]);
 
 Rx.combineLatest(streamA$, streamB$)
